fada8942907ef4adfbd2af4c208b4fb8ae6039d5
Miguel Cuello <luismiguelcm2015@gmail.com>
2023-05-23 08:59:28 -0500

    I've created the menu mobile  with Javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+const menuMobile = document.querySelector('#menu');
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+
+open.addEventListener('click', () => {
+    menuMobile.classList.add('observable');
+} );
+
+close.addEventListener('click', () => {
+    menuMobile.classList.remove('observable')
+});
