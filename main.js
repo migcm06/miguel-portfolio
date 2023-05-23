ee9a78e47de0a6d1016d206900e604e20cb2f19a
Miguel Cuello <luismiguelcm2015@gmail.com>
2023-05-23 09:23:25 -0500

    Eslint bugs solved

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const open = document.querySelector('#open');
 const close = document.querySelector('#close');
 
 open.addEventListener('click', () => {
-    menuMobile.classList.add('observable');
-} );
+  menuMobile.classList.add('observable');
+});
 
 close.addEventListener('click', () => {
-    menuMobile.classList.remove('observable')
+  menuMobile.classList.remove('observable');
 });
