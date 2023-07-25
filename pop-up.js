8fff8dd151b2bf4b77467e23c1c4d966ff10d943
Miguel Cuello <luismiguelcm2015@gmail.com>
2023-07-25 17:45:02 -0500

    I solved a problem with arrow fuction syntax

--- pop-up.js
+++ pop-up.js
@@ -53,7 +53,7 @@ function openPopup(index) {
   popUpContainer.style.display = 'block';
 
   const closeBtn = document.getElementById('close-pop');
-  closeBtn.addEventListener('click', => () {
+  closeBtn.addEventListener('click', () => {
     overlay.style.display = 'none';
     popUp.style.display = 'none';
   });
