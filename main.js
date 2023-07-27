7be79293aed10289f6faec6b59d7566f03fa57e5
Miguel Cuello <luismiguelcm2015@gmail.com>
2023-07-27 16:50:57 -0500

    I did an alert dinamically to validate lowercase email input

--- main.js
+++ main.js
@@ -16,3 +16,58 @@ links.forEach((link) => {
     menuMobile.classList.remove('observable');
   });
 });
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const nameInput = form.elements["name"];
+  const emailInput = form.elements["email"];
+  const commentInput = form.elements["comment"];
+
+  if (nameInput.value.trim() === "") {
+      showErrorMessage("Please enter your name.");
+      return;
+  }
+
+  if (!isValidEmail(emailInput.value.trim())) {
+      showErrorMessage("Please enter a valid email address.");
+      return;
+  }
+
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+      showErrorMessage("Please enter the email address in lowercase.");
+      return;
+  }
+
+  if (commentInput.value.trim() === "") {
+      showErrorMessage("Please write your message.");
+      return;
+  }
+
+  form.submit();
+});
+
+function showErrorMessage(message) {
+  const errorDiv = document.getElementById("email-error");
+  errorDiv.textContent = message;
+  errorDiv.classList.add("active");
+}
+
+function hideErrorMessage() {
+  const errorDiv = document.getElementById("email-error");
+  errorDiv.classList.remove("active");
+}
+
+// Ocultar el mensaje de error cuando el usuario comienza a editar el campo de correo electrónico.
+document.getElementById("form").addEventListener("input", function(event) {
+  const emailInput = event.target.elements["email"];
+  if (isValidEmail(emailInput.value.trim())) {
+      hideErrorMessage();
+  }
+});
