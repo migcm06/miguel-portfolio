bc212e3c467ca472f5975a71c2b8e39c810e146e
Miguel Cuello <luismiguelcm2015@gmail.com>
2023-07-28 14:40:27 -0500

    Change the position of hamburguer menu.

--- pop-up.js
+++ pop-up.js
@@ -3,25 +3,41 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       title: 'Tonic',
       imgSrc: './images/portfolio.png',
+      frame: './images/fame2.png',
       description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      description_popUp: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
       tags: ['html', 'css', 'javaScript'],
       popupIndex: 0,
     },
     {
       title: 'Multi-Post Stories',
       imgSrc: './images/portfolio2.png',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      frame: './images/fame2.png',
+      description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      description_popUp: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
       tags: ['html', 'css', 'javaScript', 'Ruby'],
       popupIndex: 1,
       isSecondProject: true,
     },
+    {
+      title: 'Facebook 360',
+      imgSrc: './images/portfolio.png',
+      frame: './images/frame3.png',
+      description: 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+      description_popUp: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+      tags: ['html', 'css', 'javaScript', 'Ruby'],
+      popupIndex: 1,
+    },
   ];
 
   const cardsSection = document.getElementById('cards');
+  const overlay = document.querySelector('.overlay');
+  const popUpContainer = document.querySelector('.pop-up-container');
+  const popUp = document.querySelector('.pop-up');
 
-  projects.forEach((project) => {
+  function createCard(project) {
     const projectCard = document.createElement('div');
-    projectCard.classList.add('grid-item');
+    projectCard.classList.add('grid-item', 'card');
     if (project.isSecondProject) {
       projectCard.classList.add('second-project');
     }
@@ -30,6 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
       <img src="${project.imgSrc}" alt="a_screenshot" class="screen-pic">
       <div class="text-card">
         <h2 class="title cards">${project.title}</h2>
+        <img src="${project.frame}" alt="date">
         <p class="text">${project.description}</p>
         <ul class="code-links">
           ${project.tags.map((tag) => `<li class="tag"><a href="#">${tag}</a></li>`).join('')}
@@ -39,67 +56,39 @@ document.addEventListener('DOMContentLoaded', () => {
     `;
 
     cardsSection.appendChild(projectCard);
+
+    projectCard.querySelector('.button-project').addEventListener('click', () => {
+      const popupHTML = `
+        <div id="close-pop"><i class="bi bi-x"></i></div>
+        <h2 class="title-pop-up">${project.title}</h2>
+        <img src="${project.frame}" class="frame-pop">
+        <div class="pop-up-img">
+          <img src="${project.imgSrc}" alt="Background image" class="pop-up-image desktop">
+        </div>
+        <div class="text-pop-up">${project.description_popUp}</div>
+        <div class="right-links">
+          <div class="tag-pop-up">${project.tags.map((tech) => `<a href="#" class="project-tech">${tech}</a>`).join('')}</div>
+          <div class="buttons-pop">
+            <a class="btn-pop" href="${project.seeLive}" target="_blank">See Live <img src="./images/Export.svg" class="icon-pop"></a>
+            <a class="btn-pop" href="${project.seeSource}" target="_blank">See Source <img src="./images/Github.svg" class="icon-pop"></a>
+          </div>
+        </div>
+      `;
+
+      popUp.innerHTML = popupHTML;
+      overlay.style.display = 'block';
+      popUp.style.display = 'block';
+      popUpContainer.style.display = 'block';
+
+      const closeBtn = document.getElementById('close-pop');
+      closeBtn.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        popUp.style.display = 'none';
+      });
+    });
+  }
+
+  projects.forEach((project) => {
+    createCard(project);
   });
 });
-
-const projects = [
-  {
-    name: 'Tonic',
-    frame: './images/fame2.png',
-    cover: './images/popUp.png',
-    textPopUp: 'Lorem ipsum dolor sit amet  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi voluptatibus consequatur iure consequuntur odit, ex ipsa debitis. Reiciendis veniam quae corporis non quis voluptatem nam maiores? Molestiae facere praesentium laborum?',
-    technologies: ['HTML', 'CSS', 'JavaScript'],
-    seeLive: 'https://example.com',
-    seeSource: 'https://github.com/migcm06/miguel-portfolio.git',
-  },
-  {
-    name: 'Multi-Post',
-    frame: './images/fame2.png',
-    cover: './images/popUp.png',
-    textPopUp: 'Lorem ipsum dolor sit amet  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi voluptatibus consequatur iure consequuntur odit, ex ipsa debitis. Reiciendis veniam quae corporis non quis voluptatem nam maiores? Molestiae facere praesentium laborum?',
-    technologies: ['HTML', 'CSS', 'JavaScript', 'Query'],
-    seeLive: 'https://example.com',
-    seeSource: 'https://github.com/migcm06/miguel-portfolio.git',
-  },
-];
-/* eslint-disable no-unused-vars */
-function openPopup(index) {
-  const overlay = document.querySelector('.overlay');
-  const popUpContainer = document.querySelector('.pop-up-container');
-  const popUp = document.querySelector('.pop-up');
-  const project = projects[index];
-  const buttons = document.querySelector('.button-project');
-
-  const popupHTML = `
-  <div id="close-pop"><i class="bi bi-x"></i></div>
-    <h2 class="title-pop-up">${project.name}</h2>
-    <img src="${project.frame}" class="frame-pop">
-    <div class="pop-up-img">
-      <img
-        src="${project.cover}"
-        alt="Background image"
-        class="pop-up-image desktop"
-      />
-    </div>
-    <div class="text-pop-up">${project.textPopUp}</div>
-    <div class="right-links">
-    <div class="tag-pop-up">${project.technologies.map((tech) => `<a href="#" class="project-tech">${tech}</a>`).join('')}</div>
-    <div class="buttons-pop">
-    <a class="btn-pop" href="${project.seeLive}" target="_blank">See Live <img src="./images/Export.svg" class="icon-pop"></a>
-    <a class="btn-pop" href="${project.seeSource}" target="_blank">See Source <img src="./images/Github.svg" class="icon-pop"></a>
-  </div>
-  </div>
-  `;
-
-  popUp.innerHTML = popupHTML;
-  overlay.style.display = 'block';
-  popUp.style.display = 'block';
-  popUpContainer.style.display = 'block';
-
-  const closeBtn = document.getElementById('close-pop');
-  closeBtn.addEventListener('click', () => {
-    overlay.style.display = 'none';
-    popUp.style.display = 'none';
-  });
-}
-/* eslint-enable no-unused-vars */
